2a27a6af7a38f248272f875907332d43eec46bd2
Nicholas Parrell <nicholas@redvanworkshop.com>
2023-02-01 11:45:31 -0500

    Initial commit

---
+++ web/frontend/components/ProductsCard.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import {
+  Card,
+  Heading,
+  TextContainer,
+  DisplayText,
+  TextStyle,
+} from "@shopify/polaris";
+import { Toast } from "@shopify/app-bridge-react";
+import { useAppQuery, useAuthenticatedFetch } from "../hooks";
+
+export function ProductsCard() {
+  const emptyToastProps = { content: null };
+  const [isLoading, setIsLoading] = useState(true);
+  const [toastProps, setToastProps] = useState(emptyToastProps);
+  const fetch = useAuthenticatedFetch();
+
+  const {
+    data,
+    refetch: refetchProductCount,
+    isLoading: isLoadingCount,
+    isRefetching: isRefetchingCount,
+  } = useAppQuery({
+    url: "/api/getSession",
+    reactQueryOptions: {
+      onSuccess: () => {
+        setIsLoading(false);
+      },
+    },
+  });
+
+  const toastMarkup = toastProps.content && !isRefetchingCount && (
+    <Toast {...toastProps} onDismiss={() => setToastProps(emptyToastProps)} />
+  );
+
+  const handlePopulate = async () => {
+    setIsLoading(true);
+    const response = await fetch("/api/orders/create");
+
+    if (response.ok) {
+      await refetchProductCount();
+      setToastProps({ content: "5 products created!" });
+    } else {
+      setIsLoading(false);
+      setToastProps({
+        content: "There was an error creating products",
+        error: true,
+      });
+    }
+  };
+
+  return (
+    <>
+      {toastMarkup}
+      <Card
+        title="Key"
+        sectioned
+        // primaryFooterAction={{
+        //   loading: isLoading,
+        // }}
+      >
+        <TextContainer spacing="loose">
+          <Heading element="h4">
+            <DisplayText size="medium">
+              <TextStyle variation="strong">
+                {isLoadingCount ? "-" : data.session.accessToken + " : "}
+              </TextStyle>
+            </DisplayText>
+          </Heading>
+        </TextContainer>
+      </Card>
+    </>
+  );
+}
---
+++ web/frontend/pages/index.jsx
@@ -0,0 +1,56 @@
+import {
+  Card,
+  Page,
+  Layout,
+  TextContainer,
+  Image,
+  Stack,
+  Link,
+  Heading,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+
+import { trophyImage } from "../assets";
+
+import { ProductsCard } from "../components";
+
+export default function HomePage() {
+  return (
+    <Page narrowWidth>
+      <TitleBar title="Red Van Workshop Order Exporter" primaryAction={null} />
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <Stack
+              wrap={false}
+              spacing="extraTight"
+              distribution="trailing"
+              alignment="center"
+            >
+              <Stack.Item fill>
+                <TextContainer spacing="loose">
+                  <Heading>You have successfully installed the Red Van Workshop Order Exporter</Heading>
+                  <p>
+                    This is the key you will need to give, along with your storefront name, when using the Red Van Workshop Order Exporter tool in your org.
+                  </p>
+                </TextContainer>
+              </Stack.Item>
+              <Stack.Item>
+                <div style={{ padding: "0 20px" }}>
+                  <Image
+                    source={trophyImage}
+                    alt="Nice work on building a Shopify app"
+                    width={120}
+                  />
+                </div>
+              </Stack.Item>
+            </Stack>
+          </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <ProductsCard />
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+}
